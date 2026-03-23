4ed3579501344fba6137dd366916d94ec927580a
Alvaro <alvaros.salgado07@gmail.com>
2026-03-23 14:14:30 -0300

    PDF 1 finalizado - Configuração de formato XML no Postman; Dependencias novas instaladas; Criação da Classe ClienteController e da Classe Cliente

--- src/main/java/br/gm/alv/OSApiApplication/api/controller/ClienteController.java
+++ src/main/java/br/gm/alv/OSApiApplication/api/controller/ClienteController.java
@@ -10,14 +10,19 @@ import java.util.ArrayList;
  *
  * @author digma
  */
-
 @RestController
 public class ClienteController {
-    
+
     List<Cliente> listaClientes;
-    
+
     @GetMapping("/clientes")
-    public String listas() {
-        listaClientes = 
+    public List<Cliente> listas() {
+
+        listaClientes = new ArrayList<Cliente>();
+        listaClientes.add(new Cliente(1, "Alvaro", "alvaro@teste.com", "11-99999-9999"));
+        listaClientes.add(new Cliente(1, "Joao", "joao@teste.com", "11-88888-9999"));
+        listaClientes.add(new Cliente(1, "Maria", "maria@teste.com", "11-77777-9999"));
+
+        return listaClientes;
     }
 }
